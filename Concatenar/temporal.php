74c6bc183086ca33bd6a207e8cacb75607dd77be
AaronVelasco93 <aaronvelasco1933@outlook.com>
2023-10-05 09:53:53 -0600

    ejemplo de clase del 05/10/2023 v1

---
+++ Concatenar/temporal.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+<body>
+<?php
+    $edad= 31;//valido
+    //$3edad=56;
+    $nombre='Aaron';//String
+    $apellido="Velasco";//String
+    $altura=1.76;//flotante
+    $mayorEdad=true;//boolean
+
+    echo $edad;
+
+    //parceo
+    $variable="10";
+    $numero=(int)$variable;
+
+    //Arreglos
+
+    $semana=[
+        'Lunes',
+        'Martes',
+        'Miercoles',
+        'Jueves',
+        'Viernes',
+        'Sabdo',
+        'Domingo'
+    ];
+    echo"<br>";
+    echo $semana[0];
+
+    echo var_dump($semana);
+    >
+    >
+    &&
+    ||
+    !
+    ==
+    ===
+    !=
+    !==
+    if(condcion){
+
+    }
+    if(condicion){
+        logica
+    }else {
+        logica
+    }
+
+0 al 3 -> Tienes que repetir la class_exists
+4 a 5 -> Echale ganitas
+5 a 6 -> Apenas y pasaste
+7 a 8 -> Very gregoriantojd
+9 a 10 -> Eres el/la mejor 
+
+
+
+
+
+
+
+
+?>
+
+
+</body>
+</html>
