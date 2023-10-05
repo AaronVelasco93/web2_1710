407638a7559e108204885d311277114908846c15
AaronVelasco93 <aaronvelasco1933@outlook.com>
2023-10-05 10:05:15 -0600

    modificacion del clase pasada

--- Concatenar/form_concatena.php
+++ Concatenar/form_concatena.php
@@ -4,20 +4,6 @@
 
     if ($dato1=="aaron"){
         echo "<h1>Hola</h1> " . $dato1;
-        echo "<h1>
-        <hr>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates velit, non amet eius sit quia quibusdam libero repudiandae veritatis obcaecati magnam quas reiciendis est, exercitationem nulla, doloremque nemo tempore aut.</p>
-    </h1>";
-
-    ?>
-        <pre>
-        hola
-        como 
-        estas 
-                    tu
-    </pre>
-    <?php
-        echo "<br>";
         echo '<a href="Formulario.php">aquí</a>';
 
     }else {
